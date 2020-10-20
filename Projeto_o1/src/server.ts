bd7bb79b3f719210e54a8090dd4ca50427735089
eurico77 <euricomagalhaes61@gmail.com>
2020-10-19 23:00:24 -0300

    configurando scafold do projeto

--- Projeto_o1/src/server.ts
+++ Projeto_o1/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import routes from './routes'
 
 const app = express();
+app.use(routes)
 app.use(express.json())
 
 
 app.listen(3333, () => {
-   console.log('Is runing')
+  console.log('Is runing')
 })
 
