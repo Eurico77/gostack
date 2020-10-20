30f48c0d6301037efb569eeac47750451eb04c45
eurico77 <euricomagalhaes61@gmail.com>
2020-10-20 20:18:00 -0300

    configuracao terminada

--- Projeto_o1/src/routes/index.ts
+++ Projeto_o1/src/routes/index.ts
@@ -2,6 +2,11 @@ import { Router } from 'express';
 
 const routes = Router();
 
+routes.post('/test', (req, res) => {
+  const { name, id } = req.body;
+  console.log(name, id)
+})
+
 export default routes;
 
 
